c0c00936b18e9107b2f4663695cab0f4eb8ed55a
“masazumi0526git config --global user.email “n_zumi529@icloud.com <n_zumi529@icloud.com>
2025-02-09 21:32:57 +0900

    /login にリダイレクト に修正

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,10 +1,12 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import SignupPage from "../pages/SignupPage";
 import LoginPage from "../pages/LoginPage";
 
 const AppRoutes = () => {
   return (
     <Routes>
+      {/* 初期ページ（"/"）にアクセスしたら "/login" にリダイレクト */}
+      <Route path="/" element={<Navigate to="/login" replace />} />
       <Route path="/signup" element={<SignupPage />} />
       <Route path="/login" element={<LoginPage />} />
     </Routes>
