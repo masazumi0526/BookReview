9b65f6ce62b86321cd2d10c5960828c825493f6b
“masazumi0526git config --global user.email “n_zumi529@icloud.com <n_zumi529@icloud.com>
2025-02-11 21:49:46 +0900

    ログイン後の遷移先変更

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -27,7 +27,7 @@ const LoginPage = () => {
         throw new Error(result.ErrorMessageJP);
       }
 
-      navigate("/dashboard"); // ダッシュボード画面に遷移
+      navigate("/public/books"); // ダッシュボード画面に遷移
     } catch (error) {
       setErrorMessage(error.message);
     }
