a1f97d4e113ad46d65750f297ce8ccaaa10ab604
“masazumi0526git config --global user.email “n_zumi529@icloud.com <n_zumi529@icloud.com>
2025-02-18 14:35:31 +0900

    ユーザー情報をローカルストレージに保存

--- src/store/authSlice.js
+++ src/store/authSlice.js
@@ -1,8 +1,8 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  user: null,
-  token: null,
+  user: JSON.parse(localStorage.getItem("user")) || null, // ローカルストレージから取得
+  token: localStorage.getItem("token") || null, // ローカルストレージから取得
 };
 
 const authSlice = createSlice({
@@ -10,20 +10,29 @@ const authSlice = createSlice({
   initialState,
   reducers: {
     login: (state, action) => {
-      state.user = action.payload.user;
-      state.token = action.payload.token;
+      state.user = action.payload.user; // ユーザー情報保存
+      state.token = action.payload.token; // トークン保存
+      localStorage.setItem("user", JSON.stringify(action.payload.user));
+      localStorage.setItem("token", action.payload.token);
+    },
+    setUser: (state, action) => {
+      state.user = action.payload;
+      localStorage.setItem("user", JSON.stringify(action.payload)); // ユーザー情報をローカルストレージに保存
     },
     setToken: (state, action) => {
       state.token = action.payload;
+      localStorage.setItem("token", action.payload); // トークンをローカルストレージに保存
     },
     logout: (state) => {
       state.user = null;
       state.token = null;
+      localStorage.removeItem("user"); // ローカルストレージから削除
+      localStorage.removeItem("token"); // ローカルストレージから削除
     },
   },
 });
 
-export const { login, setToken, logout } = authSlice.actions;
+export const { login, setUser, setToken, logout } = authSlice.actions;
 export const selectUser = (state) => state.auth.user;
 export const selectToken = (state) => state.auth.token;
 export default authSlice.reducer;
