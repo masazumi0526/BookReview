cff5196372597df9f9ba9c8bde95aa167b6c14d6
“masazumi0526git config --global user.email “n_zumi529@icloud.com <n_zumi529@icloud.com>
2025-02-22 17:38:58 +0900

    meta descriptionタグを設定

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Helmet } from "react-helmet"; // Helmetをインポート
 import { Link } from "react-router-dom";
 import "../styles/BookList.css";
 
@@ -23,4 +24,23 @@ const BookList = ({ books }) => {
   );
 };
 
-export default BookList;
+const BookListPage = ({ books }) => {
+  return (
+    <div>
+      {/* Helmetを使ってmeta descriptionを追加 */}
+      <Helmet>
+        <meta
+          name="description"
+          content="書籍一覧ページでは、最新の書籍やレビューをチェックできます。多様なジャンルの書籍をご覧ください。"
+        />
+        <title>書籍一覧</title>
+      </Helmet>
+
+      <div className="book-list-container">
+        <BookList books={books} />
+      </div>
+    </div>
+  );
+};
+
+export default BookListPage;
